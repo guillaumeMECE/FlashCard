695fcfaafc870f98322bfee937083c47aa150a42
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2018-11-13 17:41:41 +0900

    add class

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+class Card {
+   constructor(str1,str2) {
+      this.str1 = str1;
+      this.str2 = str2;
+   }
+
+   // Getter
+   getStr1(){
+      return this.str1;
+   }
+   getStr2(){
+      return this.str2;
+   }
+}
