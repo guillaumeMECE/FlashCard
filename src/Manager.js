69ee0b683fe58039a86c7752f80bedd721cc9fd8
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2018-11-15 15:21:44 +0900

    work fine instanciate and change

--- src/Manager.js
+++ src/Manager.js
@@ -3,32 +3,29 @@ class Manager {
 
       this.tabCard = []; // tab de cartes
       this.keyCard = 0; // id de la carte actuelle (connaitre notre avancé)
-      this.stringCard = 0; // 0 recto , 1 verso
-      var self = this;
+      this.stringCard = 1; // 0 recto , 1 verso
+      var self = this; // use for firebase because go out of class
       var database = firebase.database();
       database.ref('/Cards/').once('value', function(snapshot) {
-         snapshot.forEach(function(childSnapshot) {
+         snapshot.forEach(function(childSnapshot) { // shot every card
             var childKey = childSnapshot.key;
             var childData = childSnapshot.val();
-            self.add(new Card(childData.str1, childData.str2));
-            self.cardManage();
-            console.log("done");
+            self.add(new Card(childData.str1, childData.str2)); // add to tabCard
          });
+         self.cardManage(self); // replace the txt of the card
       });
    }
 
    add(other) {
-      //console.log("done");
       this.tabCard.push(other);
    }
 
    cardManage() {
       if (this.stringCard == 0) {
-         document.getElementById("cardText").innerHTML = flipCard();
+         document.getElementById("cardText").innerHTML = this.flipCard();
       } else {
-         document.getElementById("cardText").innerHTML = changeCard();
+         document.getElementById("cardText").innerHTML = this.changeCard();
       }
-
    }
    flipCard() {
       this.stringCard = 1;
@@ -36,13 +33,13 @@ class Manager {
       return this.tabCard[this.keyCard].getStr2();
    }
    changeCard() {
-      this.keyCard ++;
+      this.keyCard++;
+      this.stringCard = 0;
       document.getElementById("cardTitle").innerHTML = "Recto";
       return this.tabCard[this.keyCard].getStr1();
    }
 
    getTst() {
-      //console.log(this.tabCard);
       return this.tabCard[0].getStr1();
    }
 }
